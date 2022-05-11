8cc9f1c5e928023eb73459d95c0ee235c733a379
lucasfreittas <l.freittas@outlook.com>
2022-05-11 19:10:49 -0300

    countdown logic

--- JS/countdown.js
+++ JS/countdown.js
@@ -2,14 +2,69 @@ import * as buttons from "./buttons.js";
 
 let minutesDisplay = buttons.minutes
 let secondsDisplay = buttons.seconds
-
+let looping;
+let newMinutes;
 
 export let refreshDisplay = (minutes, seconds) => {
-    minutesDisplay.textContent = minutes
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
 }
 
 export let watch = () => {
-    let minutes = Number(prompt(`Quantos minutos você gostaria de se concentrar?`))
-    refreshDisplay(minutes, 0)
-    console.log(minutesDisplay)
+    newMinutes = Number(prompt(`Quantos minutos você gostaria de se concentrar?`))
+    let seconds = secondsDisplay.textContent
+    refreshDisplay(newMinutes, seconds)
+    return newMinutes
+}
+
+
+export let play = () => {
+   let seconds = Number(secondsDisplay.textContent)
+   let minutes = Number(minutesDisplay.textContent)
+   
+  looping = setTimeout(function(){
+    seconds = seconds - 1
+
+    if(seconds < 0){
+        seconds = 59
+        minutes = minutes -1
+    }else if(minutes == 0 && seconds == 0){
+        refreshDisplay(minutes, seconds)
+        return
+    }
+
+    refreshDisplay(minutes, seconds)
+
+
+    play()
+
+   }, 1000)
+
+}
+
+export let pause = () => {
+    clearTimeout(looping)
+}
+
+export let stop = () => {
+    clearTimeout(looping)
+    refreshDisplay(newMinutes, 0)
+}
+
+export let plusFive = () => {
+    pause()
+    newMinutes = Number(minutesDisplay.textContent) + 5
+    let seconds = secondsDisplay.textContent
+    refreshDisplay(newMinutes, seconds)
+    play()
+    
+}
+
+export let minusFive = () => {
+    pause()
+    newMinutes = Number(minutesDisplay.textContent) - 5
+    let seconds = secondsDisplay.textContent
+    refreshDisplay(newMinutes, seconds)
+    play()
+    
 }
--- JS/main.js
+++ JS/main.js
@@ -6,27 +6,36 @@ import * as countdown from "./countdown.js"
 buttons.play.addEventListener("click", function(){
     controls.play()
     controls.watch()
+    countdown.play()
 })
 
 buttons.pause.addEventListener("click", function(){
     controls.pause()
+    countdown.pause()
 })
 
 buttons.stop.addEventListener("click", function(){
     controls.stop()
     controls.pause()
+    countdown.stop()
 })
 
 buttons.watch.addEventListener("click", function(){
    countdown.watch()
+ 
 })
 
 buttons.plusTime.addEventListener("click", function(){
-    console.log("fala zé")
+    countdown.plusFive()
+    controls.play()
+    controls.watch()
+
 })
 
 buttons.minusTime.addEventListener("click", function(){
-    console.log("fala zé")
+    countdown.minusFive()
+    controls.play()
+    controls.watch()
 })
 
 buttons.florest.addEventListener("click", function(){
