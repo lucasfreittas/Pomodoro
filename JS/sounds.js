9366e6101055ced0b141743abeea5b51cbaca9f4
lucasfreittas <l.freittas@outlook.com>
2022-05-12 18:41:37 -0300

    finishing pomodore's logic

--- JS/sounds.js
+++ JS/sounds.js
@@ -1,23 +1,32 @@
 
+
 const florestSound = new Audio("./Sounds/Floresta.wav")
 const rainSound  = new Audio("./Sounds/Chuva.wav")
 const coffeSound  = new Audio("./Sounds/Cafeteria.wav")
 const fireplaceSound  = new Audio("./Sounds/Lareira.wav")
 
 export const playFlorest= () => {
-    florestSound.play()
+    florestSound.currentTime = 0;
+    florestSound.play();
+    florestSound.loop = true;
 }
 
 export const playRain= () => {
+    rainSound.currentTime = 0;
     rainSound.play()
+    rainSound.loop = true;
 }
 
 export const playCoffe= () => {
-    coffeSound.play()
+    coffeSound.currentTime = 0;
+    coffeSound.play();
+    coffeSound.loop = true;
 }
 
 export const playFireplace= () => {
+    fireplaceSound.currentTime = 0;
     fireplaceSound.play()
+    fireplaceSound.loop = true;
 }
 
 export const stopSound= () => {
